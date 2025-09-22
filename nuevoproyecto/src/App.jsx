8e4644e18ac819e49d9e22e5fde6785417d37e3c
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-09-22 10:25:29 +0200

    feat: inicializamos nuestro primer ejercicio tres en raya. Primer pasito

--- nuevoproyecto/src/App.jsx
+++ nuevoproyecto/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Square({ value }) {
+  return <button className="square">{ value }</button>;
 }
 
-export default App
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
+// el export default es como decir main, es la funcion principal es el que se va a compilar
+// la palabra export hace que sea accesible desde otros archivos
+// La primera línea define una función llamada Square. 
+// La palabra clave de JavaScript export hace que esta función sea accesible fuera de este archivo. 
+// La palabra clave default le dice a otros archivos que usan su código que es la función principal en su archivo.
