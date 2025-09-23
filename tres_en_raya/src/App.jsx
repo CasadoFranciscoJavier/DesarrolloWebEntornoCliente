b55812bb80641a8b187d5ee109fad1ced1d9af6b
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-09-23 14:15:13 +0200

    feat: nops quedamos en function handlClick

--- tres_en_raya/src/App.jsx
+++ tres_en_raya/src/App.jsx
@@ -1,25 +1,48 @@
+import { useState } from "react";
 
 function Square({ value }) {
-  return <button className="square">{ value }</button>;
+
+  //declarar una variable de estado
+  //useState crea una variable de estado y su respectivo set. el valor por defecto o inicial de la variable es el argumento que hayamos pasado al useState
+  //las variables de estado están formadas por un par variable- funcionSet
+  // const [value, setValue] = useState(null);
+
+  // function handleClick() {
+  //   setValue("X")
+  // }
+  // quitammos todo esto porque ya no hace falta al crear el array en nuestro board
+  return (
+    <button className="square">
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+  
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
